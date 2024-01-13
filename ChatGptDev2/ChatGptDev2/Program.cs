5647570f2eb860b78f766754dc136e3ad8b290f5
Batuhan Aksoy <101870674+Batuhan-Aksoy@users.noreply.github.com>
2024-01-13 20:07:55 +0300

    added project

---
+++ ChatGptDev2/ChatGptDev2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+using Azure.AI.OpenAI;
+
+//chat gpt api key
+OpenAIClient client = new OpenAIClient("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");
+
+Console.WriteLine("Chat GPT 3.5");
+
+while (true)
+{
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("You: ");
+    Console.ResetColor();
+    string input = Console.ReadLine() ?? string.Empty;
+
+    if (input.ToLower() == "exit")
+        break;
+
+    var OpenAIResponse = await client.GetChatCompletionsAsync("gpt-3.5-turbo", new ChatCompletionsOptions
+    {
+        Messages =
+    {
+        new ChatMessage(ChatRole.System,input)
+    }
+    });
+
+    foreach (var Choice in OpenAIResponse.Value.Choices)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("Chatbot: ");
+        Console.ResetColor();
+        Console.WriteLine(Choice.Message.Content);
+    }
+
+    
+}
